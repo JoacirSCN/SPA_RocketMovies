d235892c207180179d62830c494fada7660e5932
joacir <joacirprog@gmail.com>
2023-01-25 09:38:57 -0300

    Setup Initial Completed

---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#232129",
+    BACKGROUND_800: "#312E38",
+    BACKGROUND_700: "#hsla(349, 100%, 76%, 0.05)",
+
+    WHITE: "#F4EDE8",
+    PINK: "#FF859B",
+
+    GRAY_100: "##999591",
+    GRAY_300: "#666360",
+
+    RED: "#FF002E"
+  }
+}
